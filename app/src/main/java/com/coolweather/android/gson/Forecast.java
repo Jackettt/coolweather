56f6c169a5def987a992588378cadcecd802bede
Lloyd <634072204@qq.com>
2017-06-25 17:59:44 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/Forecast.java
@@ -0,0 +1,25 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by admin on 2017/6/25.
+ */
+
+public class Forecast {
+    public String date;
+    @SerializedName("tmp")
+    public Temperature temperature;
+    public class Temperature
+    {
+        public String max;
+        public String min;
+    }
+    @SerializedName("cond")
+    public More more;
+    public class More
+    {
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
